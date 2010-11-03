3eacaa0ea0c6c83fd107fef956b1e351e4b5f3c6
Edgar Simo <bobbens@gmail.com>
2010-11-03 11:46:50 +0100

    * Added doxygen file.

--- dq.h
+++ dq.h
@@ -5,29 +5,128 @@
 
 
 /**
- * Using C^{+}_{0,3,1} notation (McArthy).
+ * @file dq.h
+ */
+
+
+/**
+ * @brief Using \f$C^{+}_{0,3,1}\f$ notation (McArthy).
  *
  *    0  1  2  3  4  5  6  7
- *    1  i  j  k  e ei ej ek
+ *    1  i  j  k ei ej ek  e
  */
 typedef double dq_t[8];
 
 /* Creation. */
+/**
+ * @brief Creates a pure rotation dual quaternion.
+ *
+ *    @param[out] out Dual quaternion created.
+ *    @param zite Angle to ratet.
+ *    @param s Vector to rotate around.
+ *    @param c Any point of the vector (to create plucker coordinates).
+ */
 void dq_cr_rotation( dq_t out, double zita, double s[3], double c[3] );
+/**
+ * @brief Creates a pure translation dual quaternion.
+ *
+ *    @param[out] out Dual quaternion created.
+ *    @param t Vector to translate by.
+ */
 void dq_cr_translation( dq_t out, double t[3] );
+/**
+ * @brief Creates a point dual quaternion.
+ *
+ *    @param[out] out Dual quaternion created.
+ *    @param pos Position of the point.
+ * @sa dq_op_f4g
+ */
 void dq_cr_point( dq_t out, double pos[3] );
+/**
+ * @brief Copies a dual quaternion.
+ *
+ *    @param[out] out Dual quaternion created.
+ *    @param in Dual quaternion to copy.
+ */
 void dq_cr_copy( dq_t out, dq_t in );
+/**
+ * @brief Conjugates a dual quaternion.
+ *
+ * p+eq = p*+eq*
+ *
+ *    @param[out] out Dual quaternion created (conjugated).
+ *    @param in Dual quaternion to conjugate.
+ */
 void dq_cr_conj( dq_t out, dq_t in );
 
 /* Operations. */
+/**
+ * @brief Adds two dual quaternions.
+ * 
+ *    @param[out] The result of the addition.
+ *    @param p First quaternion to add.
+ *    @param q Second quaternion to add.
+ */
 void dq_op_add( dq_t out, dq_t in_p, dq_t in_q );
+/**
+ * @brief Multiplies to dual quaternions.
+ *
+ * pq = p * q
+ *
+ *    @param pq Result of the multiplication.
+ *    @param p First quaternion to multiply.
+ *    @param q Second quaternion to multiply.
+ */
 void dq_op_mul( dq_t pq, dq_t p, dq_t q );
+/**
+ * @brief Clifford conjugation transformation of type f2g (Alba Perez notation).
+ *
+ * \f[
+ * A |----> ABA*
+ * \f]
+ *
+ * This transformation is useful for lines.
+ *
+ *    @param ABA Result of the transformation.
+ *    @param A Dual quaternion representing the transformation.
+ *    @param B Dual quaternion being transformated.
+ */
 void dq_op_f2g( dq_t ABA, dq_t A, dq_t B );
+/**
+ * @brief Clifford conjugation transformation of type f4g (Alba Perez notation).
+ *
+ * \f[
+ * A |----> AB(a0 - a1 - a2 - a3 + e( a4 + a5 + a6 - a7 )
+ * \f]
+ *
+ * This transformation is useful for points.
+ *
+ *    @param ABA Result of the transformation.
+ *    @param A Dual quaternion representing the transformation.
+ *    @param B Dual quaternion being transformated.
+ */
 void dq_op_f4g( dq_t ABA, dq_t A, dq_t B );
 
 /* Misc. */
+/**
+ * @brief Compares two dual quaternions.
+ *
+ *    @param p First dual quaternion to compare.
+ *    @param q Second dual quaternion to compare.
+ *    @return 0 if they are equal.
+ */
 int dq_cmp( dq_t p, dq_t q );
+/**
+ * @brief Prints a quaternion on a single line.
+ * 
+ *    @param dq Dual quaternion to print.
+ */
 void dq_print( dq_t dq );
+/**
+ * @brief Prints a dual quaternion vertically.
+ * 
+ *    @param dq Dual quaternion to print.
+ */
 void dq_print_vert( dq_t dq );
 
 
--- test.c
+++ test.c
@@ -52,7 +52,7 @@ static int test_rotation (void)
    dq_t P, R, RP, PF;
 
    /* Create positions. */
-   a    = M_PI;
+   a    = M_PI/2.;
    p[0] = 1.;
    p[1] = 1.;
    p[2] = 1.;
@@ -67,9 +67,9 @@ static int test_rotation (void)
    pf[2] = 1.;
 
    /* Calculate. */
-   dq_cr_point( P, p );
-   dq_cr_rotation( R, a, s, c );
-   dq_op_f4g( RP, R, P );
+   dq_cr_point( P, p ); /* B */
+   dq_cr_rotation( R, a, s, c ); /* A */
+   dq_op_f4g( RP, R, P ); /* ABA* */
    dq_cr_point( PF, pf );
 
    /* Results. */
@@ -92,76 +92,80 @@ static int test_rotation (void)
 
 static int test_full (void)
 {
+   int i;
+   double a, p[3], t[3], s[3], c[3], pf[3];
+   dq_t P, R, T, TR, TRP, PF;
+
+   /* Create positions. */
+   a    = M_PI/2.;
+   p[0] = 0.;
+   p[1] = 0.;
+   p[2] = 0.;
+   s[0] = 1./M_SQRT2;
+   s[1] = -1./M_SQRT2;
+   s[2] = 0.;
+   c[0] = 1.;
+   c[1] = 0.;
+   c[2] = 0.;
+   t[0] = 1.;
+   t[1] = 2.;
+   t[2] = 3.;
+   pf[0] = -0.5;
+   pf[1] = -0.5;
+   pf[2] = 1./M_SQRT2;
+   for (i=0; i<3; i++)
+      pf[i] = pf[i] + t[i];
+
+
+   /* Calculate. */
+   dq_cr_point( P, p ); /* B */
+   dq_cr_rotation( R, a, s, c );
+   dq_cr_translation( T, t );
+   dq_op_mul( TR, T, R ); /* A */
+   dq_op_f4g( TRP, R, P );
+   dq_op_f4g( TRP, TR, P ); /* ABA* */
+   dq_cr_point( PF, pf );
+
+   /* Results. */
+   if (dq_cmp( PF, TRP ) != 0) {
+      fprintf( stderr, "Rotation+Translation failed!\n" );
+      printf( "Point:\n" );
+      dq_print_vert( P );
+      printf( "Rotation:\n" );
+      dq_print_vert( R );
+      printf( "Translation:\n" );
+      dq_print_vert( T );
+      printf( "Rotation+Translation:\n" );
+      dq_print_vert( TR );
+      printf( "Got:\n" );
+      dq_print_vert( TRP );
+      printf( "Expected:\n" );
+      dq_print_vert( PF );
+      return -1;
+   }
+
    return 0;
 }
 
 
 int main( int argc, char *argv[] )
 {
+   int ret;
+
    /* Warnings. */
    (void) argc;
    (void) argv;
 
-   test_translation();
-   test_rotation();
-   test_full();
+   ret  = 0;
+   ret += test_translation();
+   ret += test_rotation();
+   ret += test_full();
 
-#if 0
-   /* Create rotation. */
-   s[0] = s[2] = 0.;
-   s[2] = 1.;
-   c[0] = c[1] = c[2] = 0.;
-   dq_cr_rotation( R, M_PI_2, s, c );
-   printf("R:\n");
-   dq_print_vert( R );
+   if (ret == 0) {
+      fprintf( stdout, "All tests passed.\n" );
+   }
 
-   /* Create translation. */
-   t[0] = 1.;
-   t[1] = -1.;
-   t[2] = 0.;
-   dq_cr_translation( T, t ); 
-   printf("T:\n");
-   dq_print_vert( T );
-
-   /* Create transformation. */
-   dq_op_mul( Q, R, T );
-
-   /* Create point. */
-   d[0] = 1.;
-   d[1] = d[2] = 0.;
-   dq_cr_point( P, d );
-   printf("P:\n");
-   dq_print_vert( P );
-
-   /* Rotate. */
-   dq_op_f2g( RP, R, P );
-   printf("RP:\n");
-   dq_print_vert( RP );
-
-   /* Translate. */
-   dq_op_f2g( TRP, T, RP );
-   printf("TRP:\n");
-   dq_print_vert( TRP );
-
-   /* Translate. */
-   dq_op_f2g( TP, T, P );
-   printf("TP:\n");
-   dq_print_vert( TP );
-
-   /* Rotate. */
-   dq_op_f2g( RTP, R, TP );
-   printf("TRP:\n");
-   dq_print_vert( RTP );
-
-   /* Transform. */
-   dq_op_f2g( PQ, Q, P );
-
-   /* Display. */
-   printf("PQ:\n");
-   dq_print_vert( PQ );
-#endif
-
-   return 0;
+   return ret;
 }
 
 
