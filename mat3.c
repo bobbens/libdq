57e86bdcf50d6ed0f5b4965d3fcb06608fe5236e
Edgar Simo <bobbens@gmail.com>
2010-11-04 12:50:41 +0100

    * Added matrix functionality.

---
+++ mat3.c
@@ -0,0 +1,89 @@
+
+
+#include "mat3.h"
+
+#include <stdio.h>
+#include <math.h>
+
+
+void mat3_eye( double M[3][3] )
+{
+   M[0][0] = 1.;
+   M[0][1] = 0.;
+   M[0][2] = 0.;
+   M[1][0] = 0.;
+   M[1][1] = 1.;
+   M[1][2] = 0.;
+   M[2][0] = 0.;
+   M[2][1] = 0.;
+   M[2][2] = 1.;
+}
+
+
+double mat3_det( double M[3][3] )
+{
+   return M[0][0]*M[1][1]*M[2][2] +
+          M[1][0]*M[2][1]*M[0][2] +
+          M[2][0]*M[0][1]*M[1][2] -
+          M[0][2]*M[1][1]*M[2][0] -
+          M[0][0]*M[2][1]*M[1][2] -
+          M[1][0]*M[0][1]*M[2][2];
+}
+
+
+void mat3_inv( double out[3][3], double in[3][3] )
+{
+   double det;
+
+   det = mat3_det(in);
+
+   out[0][0] = (in[1][1]*in[2][2] - in[2][1]*in[1][2])/det;
+   out[0][1] = (in[0][2]*in[2][1] - in[2][2]*in[0][1])/det;
+   out[0][2] = (in[0][1]*in[1][2] - in[1][1]*in[0][2])/det;
+   out[1][0] = (in[1][2]*in[2][0] - in[2][2]*in[1][0])/det;
+   out[1][1] = (in[0][0]*in[2][2] - in[2][0]*in[0][2])/det;
+   out[1][2] = (in[0][2]*in[1][0] - in[1][2]*in[0][0])/det;
+   out[2][0] = (in[1][0]*in[2][1] - in[2][0]*in[1][1])/det;
+   out[2][1] = (in[0][1]*in[2][0] - in[2][1]*in[0][0])/det;
+   out[2][2] = (in[0][0]*in[1][1] - in[1][0]*in[0][1])/det;
+}
+
+
+void mat3_mul( double AB[3][3], double A[3][3], double B[3][3] )
+{
+   int c,r;
+   for (c=0; c<3; c++) {
+      for (r=0; r<3; r++) {
+         AB[r][c] = A[r][0]*B[0][c] + A[r][1]*B[1][c] + A[r][2]*B[2][c];
+      }
+   }
+}
+
+
+int mat3_comp( double A[3][3], double B[3][3] )
+{
+   int c,r, ret;
+   ret = 0;
+   for (c=0; c<3; c++) {
+      for (r=0; r<3; r++) {
+         if (fabs(A[r][c] - B[r][c]) > 1e-10) {
+            ret = ret + 1;
+         }
+      }
+   }
+   return ret;
+}
+
+
+void mat3_print( double M[3][3] )
+{
+   printf( "   % 3.3f % 3.3f % 3.3f\n"
+           "   % 3.3f % 3.3f % 3.3f\n"
+           "   % 3.3f % 3.3f % 3.3f\n",
+           M[0][0], M[0][1], M[0][2],
+           M[1][0], M[1][1], M[1][2],
+           M[2][0], M[2][1], M[2][2] );
+}
+
+
+
---
+++ mat3.h
@@ -0,0 +1,16 @@
+
+
+#ifndef _MAT3_H
+#  define _MAT3_H
+
+
+void mat3_eye( double M[3][3] );
+double mat3_det( double M[3][3] );
+void mat3_inv( double out[3][3], double in[3][3] );
+void mat3_mul( double AB[3][3], double A[3][3], double B[3][3] );
+int mat3_comp( double A[3][3], double B[3][3] );
+void mat3_print( double M[3][3] );
+
+
+#endif /* _MAT3_H */
+
